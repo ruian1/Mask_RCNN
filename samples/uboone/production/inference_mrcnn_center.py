57a2d78041398b9e20983f0ac38aae53c5e55958
Rui An <ran01@login001.lux.tufts.edu>
2019-04-08 16:24:14 -0400

    saving pdf during tufts production

--- samples/uboone/production/inference_mrcnn_center.py
+++ samples/uboone/production/inference_mrcnn_center.py
@@ -437,9 +437,10 @@ def main(IMAGE_FILE,VTX_FILE,OUT_DIR,CFG):
 
 
                         idx+=1
-                        
-                    import matplotlib.pyplot as plt
+                    
+                    import matplotlib
                     matplotlib.use('Agg')
+                    import matplotlib.pyplot as plt
 
                     fig,(ax0, ax1, ax2)=plt.subplots(1,3,figsize=(21,7))
                     visualize.display_instances(vertex_image_modified, r_center['rois'],
